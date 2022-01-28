dc34c5587105927440b82f9ebc1c3cf49a711a3a
AlexisLC <alexis.lepine.chevalier@etu.univ-st-etienne.fr>
2022-01-28 09:54:07 +0100

    form

--- src/Controller/LEPINECHEVALIERController.php
+++ src/Controller/LEPINECHEVALIERController.php
@@ -9,11 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class LEPINECHEVALIERController extends AbstractController
 {
     /**
-     * @Route("/l/e/p/i/n/e/c/h/e/v/a/l/i/e/r", name="l_e_p_i_n_e_c_h_e_v_a_l_i_e_r")
+     * @Route("/lepine", name="lepine")
      */
     public function index(): Response
     {
         return $this->render('lepinechevalier/index.html.twig', [
+            'controller_name' => 'LEPINECHEVALIERController',
+        ]);
+    }
+    /**
+     * @Route("/traitement", name="traitement")
+     */
+    public function traitement(): Response
+    {
+        return $this->render('lepinechevalier/traitement.html.twig', [
             'controller_name' => 'LEPINECHEVALIERController',
         ]);
     }
