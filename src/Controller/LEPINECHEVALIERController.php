7b76c793ca3d9a18f7f6637ddbe6db3f171c77d0
AlexisLC <alexis.lepine.chevalier@etu.univ-st-etienne.fr>
2022-02-09 17:52:42 +0100

    new user+hash

--- src/Controller/LEPINECHEVALIERController.php
+++ src/Controller/LEPINECHEVALIERController.php
@@ -18,19 +18,60 @@ class LEPINECHEVALIERController extends AbstractController
     {
         return $this->render('lepinechevalier/index.html.twig', [
             'controller_name' => 'LEPINECHEVALIERController',
+           
         ]);
     }
+    
     /**
-     * @Route("/srv/login", name="login")
+     * @Route("/newuser", name="newuser")
+     */
+    public function newuser(): Response
+    {
+        return $this->render('lepinechevalier/newuser.html.twig', [
+            'controller_name' => 'LEPINECHEVALIERController',
+           
+        ]);
+    }
+    
+
+
+    /**
+    * @Route("/creeruti", name="creeruti")
+    */
+    public function creeruti(Request $request, EntityManagerInterface $manager): Response
+    { 
+        $login = $request->get("pseudo");
+        $password = $request->request->get("pass");
+        $password=password_hash($password, PASSWORD_DEFAULT);
+
+        $monUtilisateur = new Utilisateur ();
+        $monUtilisateur -> setLogin($login);
+        $monUtilisateur -> setPassword($password);
+        $monUtilisateur -> setNom($login);
+        $manager -> persist($monUtilisateur);
+
+        $manager -> flush ();
+
+        return $this->redirectToRoute ('newuser');
+        
+    }
+    /**
+     * @Route("/login", name="login")
      */
     public function login(Request $request, EntityManagerInterface $manager): Response
     {
-        $login = $request->get("login");
-        $password = $request->request->get("password");
-        if ($login=="root" && $password="toor")
-            $message = "connexion réussite";
+        $login = $request->get("pseudo");
+        $password = $request->request->get("pass");
+    
+        $reponse = $manager -> getRepository(Utilisateur :: class) -> findOneBy([ 'login' => $login]);
+        if($reponse==NULL)
+            $message = 'Utilisateur inconnu';
         else
-        $message = "erreur d'identification";
+            $hash=($reponse ->getPassword());
+            if (password_verify($password, $hash))
+                $message = "connexion réussite";    
+            else
+             $message = "erreur de mot de passe";
         return $this->render('lepinechevalier/login.html.twig',
         [
             'login' =>$login,
